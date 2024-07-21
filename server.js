3484d73fb10cf6dca2bce8522c10155849a3259f
Melvin <melvin.sierraic3@gmail.com>
2024-07-20 23:14:24 -0400

    Adding connected to mongoDB console log inside of the then next to mongoose.connect and created model folder for schemas

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // ROUTES
 
+// GETS
 app.get('/', (req, res)=> {
     res.send("Hello node API");
 })
@@ -12,6 +14,17 @@ app.get('/blog', (req, res)=> {
 })
 
 
-app.listen(3000, ()=>{
-    console.log('Node API app is running on port 3000')
-})
+
+
+mongoose.set("strictQuery", false);
+mongoose.
+connect('mongodb+srv://admin:admin@csc-318-project.ywpdsjw.mongodb.net/Node-API?retryWrites=true&w=majority&appName=CSC-318-Project')
+.then(()=>{
+    console.log("Connected to mongoDB");
+    app.listen(3000, ()=>{
+        console.log('Node API app is running on port 3000');
+    })
+
+}).catch((error)=> {
+    console.log(error);
+});
