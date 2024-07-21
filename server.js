3816093fe165cef20fbbb6226d53c2c08fffb535
Melvin <melvin.sierraic3@gmail.com>
2024-07-20 23:46:47 -0400

    Added post for /user

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./models/userModel')
 const app = express();
 
-// ROUTES
+
+
+app.use(express.json());
+
+// ROOT ROUTES
 
 // GETS
 app.get('/', (req, res)=> {
     res.send("Hello node API");
 })
 
-app.get('/blog', (req, res)=> {
-    res.send("Hello blog");
-})
+// USER ROUTES
 
+// POSTS
+
+app.post('/user', async(req, res) =>{
+    try {
+        const user = await User.create(req.body);
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(err.message);
+        res.status(500).json({message: error.message});
+    }
+})
 
 
 
