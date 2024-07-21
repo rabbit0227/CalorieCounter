6475b8c932b419e77b81e8a70b72fda22909d81a
Melvin <melvin.sierraic3@gmail.com>
2024-07-20 21:32:59 -0400

    Installed nodemon

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/', (req, res)=> {
     res.send("Hello node API");
 })
 
+app.get('/blog', (req, res)=> {
+    res.send("Hello blog");
+})
+
+
 app.listen(3000, ()=>{
     console.log('Node API app is running on port 3000')
 })
